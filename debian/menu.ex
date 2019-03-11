0d3ab4f4e8de03322b42b548155911d3ab8ebc65
Matthias Urlichs <matthias@urlichs.de>
2019-03-11 16:14:35 +0100

    toss stupid example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rkflashtool):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rkflashtool" command="/usr/bin/rkflashtool"
--- debian/rkflashtool.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rkflashtool package
-#
-0 4	* * *	root	[ -x /usr/bin/rkflashtool_maintenance ] && /usr/bin/rkflashtool_maintenance
